f666b9171dc870b1d3fb8f8dc12f3e1f8a537f6a
LeeChangHoon <changhoon1030@gmail.com>
2022-11-13 02:58:54 +0900

    ⭐ feat : Recoil, React-Query setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,16 @@
 import type { AppProps } from 'next/app';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { RecoilRoot } from 'recoil';
+import { GlobalStyles } from '../src/styles/GlobalStyles';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <GlobalStyles/>
+      <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
+  )
 }
