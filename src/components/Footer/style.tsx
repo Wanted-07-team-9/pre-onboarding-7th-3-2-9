2a67059a20c7eeb7c37885546b51c7b8a4ab24e5
LeeChangHoon <changhoon1030@gmail.com>
2022-11-14 13:08:53 +0900

    ⭐ feat : footer 컴포넌트 추가

---
+++ src/components/Footer/style.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const FooterLayout = styled.footer` 
+  width:100%;
+  background-color: var(--color-white);
+  height: 3.5rem;
+  position: absolute;
+  bottom:0;
+  left:0;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+`
