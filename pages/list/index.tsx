f5e7f907c0a17b66b1fa3cfbe70ddbba921add6c
LeeChangHoon <changhoon1030@gmail.com>
2022-11-16 03:43:15 +0900

    ⭐ feat : 계좌 리스트 pagination 추가

--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -1,22 +1,25 @@
-import styled from "styled-components"
 import Header from "../../src/components/Header"
 import Sider from "../../src/components/Sider"
 import Footer from "../../src/components/Footer"
+import Pagination from "../../src/components/Pagination"
 import { useRecoilValueLoadable } from "recoil"
 import { accountList } from "../../src/recoil/selector/accountList"
 import Table from "../../src/components/Table"
+import { useState } from "react"
+import {Container, FixedWrapper, ContentWrapper, TableWrapper} from './style'
 
-const columns = [
-  '증권사','계좌번호','계좌상태','계좌명','평가금액','입금금액','계좌활성화여부','계좌개설일'
-]
 
 const List = () => {
-  const accountLoadable = useRecoilValueLoadable(accountList)
+  const columns = [
+    '증권사','계좌번호','계좌상태','계좌명','평가금액','입금금액','계좌활성화여부','계좌개설일'
+  ]
+  const [page, setPage] = useState(1)
+  const accountLoadable = useRecoilValueLoadable(accountList(page))
   let accounts = []
   let content = null
   switch (accountLoadable.state) {
     case 'hasValue':
-      accounts = accountLoadable.contents
+      accounts = accountLoadable.contents.accountData
       if (accounts.length === 0) {
         content = <div>로딩</div>
       } else {
@@ -41,6 +44,7 @@ const List = () => {
         <TableWrapper>
         <div>{content}</div>
         </TableWrapper>
+        <Pagination total={accountLoadable?.contents.totalData} page={page} setPage={setPage}/>
         </FixedWrapper>
         <Footer />
       </ContentWrapper>
@@ -48,24 +52,5 @@ const List = () => {
   )
 }
 
-const Container = styled.div`
-  display:flex;
-  flex-direction: row;
-`
 
-const FixedWrapper = styled.section`
-  padding-bottom : 3rem;
-`
-
-
-const ContentWrapper = styled.div`
-  min-height:100%;
-  position:relative;
-  width:100%;
-`
-
-const TableWrapper = styled.section`
-  background-color: #FAFAFA;
-  
-`
 export default List
