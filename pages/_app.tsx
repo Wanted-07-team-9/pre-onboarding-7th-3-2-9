b62a0feee653649663f991335626a7fdbb4099c6
LeeChangHoon <changhoon1030@gmail.com>
2022-11-16 03:51:56 +0900

    ⭐ feat :react-query 설정 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
+import React from 'react';
 import type { AppProps } from 'next/app';
-import { QueryClientProvider, QueryClient } from 'react-query';
+import { DehydratedState, Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 import { RecoilRoot } from 'recoil';
 import { GlobalStyles } from '../src/styles/GlobalStyles';
+// import { ReactQueryDevtools } from "react-query/devtools";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient()
+
+export default function App({ Component, pageProps }: AppProps<{ dehydratedState: DehydratedState }>) {
+  const [queryClient] = React.useState(() => new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <GlobalStyles/>
-      <Component {...pageProps} />
-      </RecoilRoot>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </Hydrate>
     </QueryClientProvider>
+
   )
 }
