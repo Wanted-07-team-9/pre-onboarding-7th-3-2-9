140462598116ffd6248729de447eb3cac828c369
LeeChangHoon <changhoon1030@gmail.com>
2022-11-17 03:25:47 +0900

    🛠 fix : query키에 page를 넣어서 page가 바뀌면 페이지네이션 되도록 수정

--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -2,49 +2,46 @@ import Header from "../../src/components/Header"
 import Sider from "../../src/components/Sider"
 import Footer from "../../src/components/Footer"
 import Pagination from "../../src/components/Pagination"
-import { useRecoilValueLoadable } from "recoil"
-import { accountList } from "../../src/recoil/selector/accountList"
 import Table from "../../src/components/Table"
 import { useState } from "react"
-import {Container, FixedWrapper, ContentWrapper, TableWrapper} from './style'
-
+import { Container, FixedWrapper, ContentWrapper, TableWrapper } from './style'
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { fetchAccount } from "../../src/api/api"
 
 const List = () => {
+  const queryClient = useQueryClient()
   const columns = [
-    '증권사','계좌번호','계좌상태','계좌명','평가금액','입금금액','계좌활성화여부','계좌개설일'
+    '증권사', '계좌번호', '계좌상태', '계좌명', '평가금액', '입금금액', '수익률', '계좌활성화여부', '계좌개설일'
   ]
   const [page, setPage] = useState(1)
-  const accountLoadable = useRecoilValueLoadable(accountList(page))
-  let accounts = []
-  let content = null
-  switch (accountLoadable.state) {
-    case 'hasValue':
-      accounts = accountLoadable.contents.accountData
-      if (accounts.length === 0) {
-        content = <div>로딩</div>
-      } else {
-        content = <Table columns={columns}  data={accounts} isAccount={true}/> 
-      }
-      break;
-    case 'hasError':
-      content =  <div>로딩</div>
-      break;
-    case 'loading':
-      content = <div>로딩</div>
-      break;
-    default:
-      content =  <div>로딩</div>
+  const { data, isLoading, isError } = useQuery(
+    ['accountList', page], () => fetchAccount(page)
+  )
+  const handlePage = (target) => {
+    setPage(target)
   }
-  return(
+  const {mutate} = useMutation(handlePage,{
+    onSuccess : () =>queryClient.invalidateQueries(['accountList'])
+  })
+  return (
     <Container>
       <Sider />
       <ContentWrapper>
         <FixedWrapper>
-        <Header pageName={'투자계획'}/>
-        <TableWrapper>
-        <div>{content}</div>
-        </TableWrapper>
-        <Pagination total={accountLoadable?.contents.totalData} page={page} setPage={setPage}/>
+          <Header pageName={'투자계획'} />
+          {isLoading && <div>loading</div>}
+          {isError && <div>error</div>}
+          {data &&
+            (
+              <>
+                <TableWrapper>
+                  <Table columns={columns} data={data.accountData} isAccount={true} />
+                </TableWrapper>
+                <Pagination total={data.totalData} page={page} setPage={setPage} /></>
+            )
+          }
+
+
         </FixedWrapper>
         <Footer />
       </ContentWrapper>
--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -2,39 +2,20 @@ import Footer from "../../src/components/Footer"
 import Header from "../../src/components/Header"
 import Sider from "../../src/components/Sider"
 import Pagination from "../../src/components/Pagination"
-import { useRecoilValueLoadable } from "recoil"
 import Table from "../../src/components/Table"
 import { useState } from "react"
-import { userList } from "../../src/recoil/selector/userList"
 import {Container, FixedWrapper, ContentWrapper, TableWrapper} from './style'
+import { useQuery } from "@tanstack/react-query"
+import { fetchUser } from "../../src/api/api"
 
 const User = () => {
   const columns= [
     '고객명', '이메일 주소','주민등록상 성별코드', '생년월일', '휴대폰 번호 ', '최근로그인',  '가입일'
   ]
   const [page, setPage] = useState(1)
-  const userLodable = useRecoilValueLoadable(userList(page))
-  let users = []
-  let content = null
-
-  switch (userLodable.state) {
-    case 'hasValue':
-      users = userLodable.contents.userData
-      if (users.length === 0) {
-        content = <div>로딩</div>
-      } else {
-        content = <Table columns={columns}  data={users} isAccount={false}/> 
-      }
-      break;
-    case 'hasError':
-      content =  <div>로딩</div>
-      break;
-    case 'loading':
-      content = <div>로딩</div>
-      break;
-    default:
-      content =  <div>로딩</div>
-  }
+  const { data, isLoading, isError } = useQuery(
+    ['userList', page], () => fetchUser(page)
+  )
 
   return(
     <Container>
@@ -42,10 +23,17 @@ const User = () => {
       <ContentWrapper>
         <FixedWrapper>
         <Header pageName={'님의 계좌 목록'}/>
-        <TableWrapper>
-          <div>{content}</div>
-        </TableWrapper>
-        <Pagination total={userLodable?.contents.totalData} page={page} setPage={setPage}/>
+        {isLoading && <div>loading</div>}
+          {isError && <div>error</div>}
+          {data &&
+            (
+              <>
+                <TableWrapper>
+                  <Table columns={columns} data={data.userData} isAccount={false} />
+                </TableWrapper>
+                <Pagination total={data.totalData} page={page} setPage={setPage} /></>
+            )
+          }
         </FixedWrapper>
         <Footer />
       </ContentWrapper>
