d51b9210169474523263e815c092d0f33a0fa832
LeeChangHoon <changhoon1030@gmail.com>
2022-11-17 03:27:38 +0900

    ⭐ feat : 수익률에 따라 글자 색상 변화

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Link from "next/link"
 import { convertBrokerId, convertAccountStatus, convertAccountNumber, convertComma, convertIsoToTimeStamp, convertPhoneNumber } from '../../utils/convertFn';
+import styled from "styled-components";
 
 const Table = ({ columns, data, isAccount }: any) => {
+
   return (
     <table>
       <thead>
@@ -16,7 +18,7 @@ const Table = ({ columns, data, isAccount }: any) => {
         {isAccount ? (
           data.map((account) => (
             <tr key={account.id}>
-              <Link href={`/list/${account.broker_id}`}>
+              <Link href={`/list/${account.id}`}>
               <a><td>{convertBrokerId(account.broker_id)}</td></a>
               </Link>
               <td>{convertAccountNumber(account.number)}</td>
@@ -24,6 +26,8 @@ const Table = ({ columns, data, isAccount }: any) => {
               <td>{account.name}</td>
               <td>{convertComma(account.assets)}</td>
               <td>{convertComma(account.payments)}</td>
+              <StyledTd className={account.assets-account.payments  >= 0 ? (account.assets - account.payments === 0 ? 'zeor' : 'plus'): 'minus'
+                }>{convertComma(account.assets-account.payments)}</StyledTd>
               <td>{account.is_active ? 'O' : 'X'}</td>
               <td>{convertIsoToTimeStamp(account.created_at)}</td>
             </tr>
@@ -45,5 +49,17 @@ const Table = ({ columns, data, isAccount }: any) => {
     </table>
   );
 }
+
+const StyledTd = styled.div`
+  &.zero{
+    color: black;
+  }
+  &.plus{
+    color : red;
+  }
+  &.minus{
+    color : blue;
+  }
+`
 
 export default Table;
