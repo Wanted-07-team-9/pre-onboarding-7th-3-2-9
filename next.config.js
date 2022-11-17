a937bd0e522dad4782f4fdcb6383eb2e9e54d850
Jun_53 <58911113+jun-05@users.noreply.github.com>
2022-11-18 03:20:37 +0900

    👏 chore : devTool Lib 설치 및 rewrite 설정 변경 - 리액트 쿼리 데브 툴을 추가하였습니다. - 동적 라우팅이 잘 안되는 이슈가있어, nextConfig에 fallback을 추가하엿습니다.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: 'http://localhost:4000/:path*',
+        },
+      ],
+    };
+  },
 };
 
 module.exports = nextConfig;
