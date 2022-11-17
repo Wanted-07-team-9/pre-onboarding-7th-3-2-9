e0b6d704d2bd48dba894813bd6ab4ae61942b08b
Jun_53 <58911113+jun-05@users.noreply.github.com>
2022-11-18 03:30:13 +0900

    ⭐ feat : type 파일 추가 - user,account,auth에 대한 type을 추가하였습니다.

---
+++ src/types/authType.ts
@@ -0,0 +1,12 @@
+export type loginForm ={
+    email :string;
+    password : string;
+}
+
+export type adminUser = {
+    accessToken:string;
+    user?:{
+        email:string;
+        id:number
+    }
+}
