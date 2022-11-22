e3a239db036e5804fd30df5784b31a4d6ea13ea4
LeeChangHoon <changhoon1030@gmail.com>
2022-11-23 02:54:47 +0900

    ⭐ feat : type interface 추가

--- src/types/interfaces.tsx
+++ src/types/interfaces.tsx
@@ -4,7 +4,7 @@ export interface IForm {
 }
 
 export interface IAccount {
-  id: number;
+  id: string;
   assets : string;
   broker_id : string;
   created_at : string;
@@ -12,8 +12,59 @@ export interface IAccount {
   name : string;
   number : string;
   payments : string;
-  status : number;
+  status : string;
   updated_at : string;
   user_id : number;
   uuid : number;
+}
+
+export interface IServerSideProps {
+  id: number
+}
+
+export interface IEditAccount{
+  assets : string;
+  broker_id : string;
+  is_active : string;
+  name : string;
+  number : string;
+  payments : string;
+  status : string
+}
+
+export interface ICreateAccount {
+  broker_id : string;
+  assets : string;
+  is_active : string | boolean;
+  name : string;
+  number : string;
+  payments : string;
+  status : string
+  created_at ?: object;
+}
+
+export interface IBrokerOption {
+  brokerCode : string;
+  brokerName : string
+}
+
+export interface IObjectKey {
+  [key:string] : string;
+}
+
+export interface IUser{
+  address : string;
+  age : number;
+  birth_date : string;
+  created_at : string;
+  detail_address : string;
+  email : string;
+  gender_origin : number;
+  id : number;
+  last_login : string;
+  name : string;
+  phone_number : string;
+  photo : string;
+  updated_at : string;
+  uuid : string;
 }
